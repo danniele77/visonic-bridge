c824b23cfae00ae6fc4e07cd3dc742c7a92ad490
danniele77 <135849775+danniele77@users.noreply.github.com>
2025-07-24 19:45:36 +0200

    Update alarm_control_panel.py

--- custom_components/visonic/alarm_control_panel.py
+++ custom_components/visonic/alarm_control_panel.py
@@ -1,12 +1,14 @@
-"""Support for Visonic alarm control panel."""
-
+import requests
+import logging
 from homeassistant.components.alarm_control_panel import AlarmControlPanelEntity
 from homeassistant.const import STATE_ALARM_ARMED_AWAY, STATE_ALARM_DISARMED
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    async_add_entities([VisonicAlarm()])
+_LOGGER = logging.getLogger(__name__)
 
-class VisonicAlarm(AlarmControlPanelEntity):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    async_add_entities([VisonicBridgeAlarm()])
+
+class VisonicBridgeAlarm(AlarmControlPanelEntity):
     def __init__(self):
         self._state = STATE_ALARM_DISARMED
 
@@ -18,10 +20,16 @@ class VisonicAlarm(AlarmControlPanelEntity):
     def state(self):
         return self._state
 
-    async def async_alarm_arm_away(self, code=None):
-        self._state = STATE_ALARM_ARMED_AWAY
-        self.async_write_ha_state()
+    def alarm_arm_away(self, code=None):
+        try:
+            requests.get("http://IP_DEL_BRIDGE:PORT/arm")
+            self._state = STATE_ALARM_ARMED_AWAY
+        except Exception as e:
+            _LOGGER.error(f"Error arming: {e}")
 
-    async def async_alarm_disarm(self, code=None):
-        self._state = STATE_ALARM_DISARMED
-        self.async_write_ha_state()
+    def alarm_disarm(self, code=None):
+        try:
+            requests.get("http://IP_DEL_BRIDGE:PORT/disarm")
+            self._state = STATE_ALARM_DISARMED
+        except Exception as e:
+            _LOGGER.error(f"Error disarming: {e}")
